8278dd02a83ff914bb1b81e2e86dee9c1db0b9b3
tyamagis <tyamagis@outlook.com>
2022-06-15 22:34:47 +0900

    FIX include path

--- libft/libft.h
+++ libft/libft.h
@@ -6,14 +6,14 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 20:54:48 by tyamagis          #+#    #+#             */
-/*   Updated: 2022/06/15 20:56:23 by tyamagis         ###   ########.fr       */
+/*   Updated: 2022/06/15 22:34:00 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include "ft_list/ft_list.h"
-#include "ft_btree/ft_btree.h"
+#include "ft_list.h"
+#include "ft_btree.h"
 
 #endif
